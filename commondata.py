ede4cc4cf8b1e4b270a2e597bdc0c835c5030dfa
Ulf Kemmsies <ulfkemmsies@gmail.com>
2021-05-26 11:05:40 +0200

    early GUI

--- commondata.py
+++ commondata.py
@@ -1,7 +1,16 @@
+import pandas as pd
+from csv import writer
+import csv
+
+
 class CommonData():
+
     
     def __init__(self) -> None:
         
+        self.read_csv('data.csv')
+        self.df_tab_organizer()
+
         self.launch_cost = 250000 #$/kg
 
         self.moon_gravity = 1.625
@@ -25,5 +34,37 @@ class CommonData():
         self.kevlar_breaking_tenacity = 3000 #MPa
         self.kevlar_dose_reduction = 5.5 #% per g/cm2
         self.kevlar_thermal_conductivity = 0.04 #W/mK
-        self.kevlar_density = 1.47 #kg/m3
+        self.kevlar_density = 1470 #kg/m3
         self.kevlar_safety_factor = 4 
+
+    def read_csv(self, file_name):
+        self.df = pd.read_csv(file_name)
+
+    def append_to_df(self, new_row):
+        self.df = self.df.append(new_row, ignore_index=True)
+
+    def df_to_csv(self, file_name):
+        out_file = self.df.to_csv(file_name, index=False)
+
+    def df_tab_organizer(self):
+        self.tab_names = self.df.group.unique()
+
+    def df_subtab_creator(self,tab_name):
+        self.subtabs = self.df.loc[self.df['group']== tab_name].object.unique()
+
+    def param_getter(self, tab_name, subtab_name):
+        filtered = self.df.loc[(self.df['group']== tab_name) & (self.df['object']== subtab_name)]
+        return filtered
+
+    def add_row(self, tab_name, subtab_name, param, value, unit):
+        new_row = {'group':tab_name, 'object':subtab_name, 'param':param, 'value':value, 'units':unit}
+        self.df = self.df.append(new_row, ignore_index=True)
+
+    def get_tab_from_subtab(self, subtab_name):
+        filtered = self.df.loc[self.df['object']== subtab_name]
+        return filtered.group.values[0]
+
+    def reverse_name_cleaner(self, entry):
+        out = entry.replace(" ", "_")
+        out = out.lower()
+        return out
