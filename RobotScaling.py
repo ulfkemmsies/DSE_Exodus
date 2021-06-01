9d12343d96caaf2ff1c1e553efd89010b5f649d3
Lian <l.greijn@student.tudelft.nl>
2021-06-01 16:38:08 +0200

    Code update (it works this time)

--- RobotScaling.py
+++ RobotScaling.py
@@ -13,22 +13,32 @@ class Robots():
     def __init__(self):
         self.data = CommonData()
         print(self.data.rassor__capacity)
+
+        #place holders required
         self.bagsize = 40 # [L] bagsize 
         self.underneathhabitat = 83.7*self.data.regolith__density #[m3] volume under habitat
-        self.total_calcu
 
+        self.total_calc()
+
+    #Calculations for the rassor
     def rassor_amount_under_habitat(self, time):
         self.robots_needed = m.ceil((self.underneathhabitat / (self.data.rassor__capacity*time))) #number of robots needed for this section
+        print("number of robots under habitat", self.robots_needed)
 
     def rassor_amount_at_excavationloc (self, time):
-        self.robots_needed2 = m.ceil((self.data.regolith__total_mass - self.underneathhabitat) / (self.data.rassor__capacity*time))
+        self.robots_needed = m.ceil((self.data.regolith__total_mass - self.underneathhabitat) / (self.data.rassor__capacity*time))
+        print("number of robots for other excavation", self.robots_needed)
 
+    #Calculations Crane
+    def crane_calculations(self, time):
+        self.bag_number_required = self.data.regolith__total_volume / self.bagsize
+        self.cranes_needed = m.ceil((self.data.crane__operating_speed * self.bag_number_required)/(time))
+        print("number of cranes needed", self.cranes_needed)
 
-    def total_calcu(self):
-        self.rassor_amount_at_excavationloc(100)
-        self.rassor_amount_under_habitat(200)
+    def total_calc(self):
+        self.rassor_amount_under_habitat(100)
+        self.rassor_amount_at_excavationloc(200)
+        self.crane_calculations(100)
 
-
-
-    
+    #Calculations Transporter
 Test = Robots()
