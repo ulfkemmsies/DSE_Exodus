77eacfe0b05043685b9e2dda0dcb4a9acd96702a
Lian <l.greijn@student.tudelft.nl>
2021-06-01 15:24:57 +0200

    New data

--- RobotScaling.py
+++ RobotScaling.py
@@ -7,13 +7,18 @@ Created on Tue May 18 15:43:41 2021
 #from DSE_Exodus.commondata import CommonData
 import math as m
 import numpy as np
-'''
+
+mrego = 1019588             #total kg required for habitat
+bagsize = 40                #[L] size of the bags
+underneathhabitat = 83.7    #[m3] volume under habitat
 class Robots():
-    def __init__(self, time):
+    def __init__(self):
         self.data = CommonData()
-        self.excavation_time = time
+        
+
+    def excavation_time_underneath():
+        self.
+
+
     
-    def truetime_Collecting(self):
-        self.true_time = self.excavation_time - (2*self.distance_hab2exc*)
-        print(Result)
-'''
+    
