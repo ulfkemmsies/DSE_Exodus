e9747faec75938bc1c1d09d6808d1c593f58a94d
Lian <l.greijn@student.tudelft.nl>
2021-06-01 16:14:57 +0200

    Another update

--- RobotScaling.py
+++ RobotScaling.py
@@ -9,16 +9,25 @@ import math as m
 import numpy as np
 from commondata import CommonData
 
-mrego = 1019588             #total kg required for habitat
-bagsize = 40                #[L] size of the bags
-underneathhabitat = 83.7    #[m3] volume under habitat
 class Robots():
     def __init__(self):
         self.data = CommonData()
         print(self.data.rassor__capacity)
+        self.bagsize = 40 # [L] bagsize 
+        self.underneathhabitat = 83.7*self.data.regolith__density #[m3] volume under habitat
+        self.total_calcu
 
-    def excavation_time_underneath(self):
-        print(self.data.rassor__capacity)
+    def rassor_amount_under_habitat(self, time):
+        self.robots_needed = m.ceil((self.underneathhabitat / (self.data.rassor__capacity*time))) #number of robots needed for this section
+
+    def rassor_amount_at_excavationloc (self, time):
+        self.robots_needed2 = m.ceil((self.data.regolith__total_mass - self.underneathhabitat) / (self.data.rassor__capacity*time))
+
+
+    def total_calcu(self):
+        self.rassor_amount_at_excavationloc(100)
+        self.rassor_amount_under_habitat(200)
+
 
 
     
