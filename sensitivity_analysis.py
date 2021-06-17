4e990c465216e7b03843073c45bb60a39ff54075
Ulf Kemmsies <ulfkemmsies@gmail.com>
2021-06-17 15:41:46 +0200

    changed plots

---
+++ sensitivity_analysis.py
@@ -0,0 +1,18 @@
+import numpy as np
+import scipy as sp
+from scipy import stats
+import matplotlib.pyplot as plt
+from commondata import CommonData
+from RobotScaling import Robots
+from Solar_Position_Optimization import PVArrays
+from structural_calculation import Structure
+from life_support import Life_Support
+import unittest
+from bisect import bisect_left
+import time
+
+class Sensitivity_Anal():
+    def __init__(self):
+        pass
+
+    
